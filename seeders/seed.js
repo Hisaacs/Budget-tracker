1f3eba628b54bae1b84fa82b8aec4be4bd4e081b
Hamish Isaacs <hamisaacs@gmail.com>
2021-03-31 21:58:01 +0800

    Add database folder to connec.js and created  .env

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,10 +1,11 @@
 var mongoose = require("mongoose");
 var db = require("../models");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+// });
+connect();
 
 var transactionSeed = [
   {
--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
+const logger = require("morgan");
+const {connect} = require('./database/connect')
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
 
+connect();
 const app = express();
-
-app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(logger("dev"));
 
 app.use(express.static("public"));
 
 // routes
 app.use(require("./routes/api.js"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("we are connected");
 });
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}! Visit http://localhost:${PORT}/ in your browser`);
